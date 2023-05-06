83e04c42377518c7377f18a3ea7dc06d8646b706
Marwan <marwansaad16@gmail.com>
2023-05-06 17:01:01 +0300

    Solved the build issues

--- build/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const tslib_1 = require("tslib");
-const serverless_http_1 = tslib_1.__importDefault(require("serverless-http"));
-const app_1 = require("./app");
-exports.handler = (0, serverless_http_1.default)(app_1.app);
