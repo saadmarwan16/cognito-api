b157290aafadb53b5bd9317092e877351a5dbda4
Marwan <marwansaad16@gmail.com>
2023-05-09 13:14:10 +0300

    Initial development commit

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,45 @@
-import express, { json, urlencoded, Request, Response } from 'express';
+import express, {
+	json,
+	urlencoded,
+	Request,
+	Response,
+	NextFunction,
+} from 'express';
 import { RegisterRoutes } from '../build/routes';
+// import { serve, setup } from 'swagger-ui-express';
 import { serve, generateHTML } from 'swagger-ui-express';
+import docs from '../build/swagger.json';
 
 export const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-app.use('/docs', serve, async (_req: Request, res: Response) => {
-	return res.send(generateHTML(await import('../build/swagger.json')));
+// app.use(
+// 	'/docs',
+// 	serve,
+// 	setup(docs, {
+// 		customSiteTitle: 'Kuumba Documentation',
+// 		customfavIcon: 'url',
+// 		swaggerOptions: { validatorUrl: null },
+// 	})
+// );
+
+app.use('/docs', serve, (_req: Request, res: Response) => {
+	return res.send(generateHTML(docs));
 });
 
 RegisterRoutes(app);
 
-app.use((_req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((_req: Request, res: Response, _next: NextFunction) => {
 	return res.status(404).send({ status: 'not found' });
 });
 
-app.use((_err: Error, _req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((_err: Error, _req: Request, res: Response, _next: NextFunction) => {
 	return res.status(500).json({
-		message: 'Some really unexpected happened',
+		message: 'Something really unexpected happened',
 	});
 });
 
