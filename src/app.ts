4e37273d99ba85a3ff65fd2c488994cd41564e77
Marwan <marwansaad16@gmail.com>
2023-05-14 14:27:53 +0300

    Changed the github actions workflow

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 import express, { json, urlencoded, Request, Response } from 'express';
 import { RegisterRoutes } from '../build/routes';
+// import { serve, setup } from 'swagger-ui-express';
 import { serve, generateHTML } from 'swagger-ui-express';
+import docs from '../build/swagger.json';
 
 export const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-app.use('/docs', serve, async (_req: Request, res: Response) => {
-	return res.send(generateHTML(await import('../build/swagger.json')));
+// app.use(
+// 	'/docs',
+// 	serve,
+// 	setup(docs, {
+// 		customSiteTitle: 'Kuumba Documentation',
+// 		customfavIcon: 'url',
+// 		swaggerOptions: { validatorUrl: null },
+// 	})
+// );
+
+app.use('/docs', serve, (_req: Request, res: Response) => {
+	return res.send(generateHTML(docs));
 });
 
 RegisterRoutes(app);
